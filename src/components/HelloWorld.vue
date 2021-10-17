dfd2fe1c7dec02dc21c21973a5f020fc76f6f349
Sean McFadden <smcfadden150@gmail.com>
2021-10-17 18:56:31 -0400

    Base Electron Vue Commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="ass">
+    woah this sucks ass
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.ass {
+  background-color: cyan;
+}
+</style>
